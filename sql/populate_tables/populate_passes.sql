6d27c5fb33e71cb89d35f60022bfd84620bc7ac5
Wyatt Griffith <svwyattski@gmail.com>
2021-10-20 18:28:31 -0600

    Populating passes table with multipasses

---
+++ sql/populate_tables/populate_passes.sql
@@ -0,0 +1,11 @@
+INSERT INTO passes
+VALUES (DEFAULT, "Ikon","Full",1149 ),(DEFAULT, "Ikon","Base",879),(DEFAULT, "Ikon","Base Plus",1029),(DEFAULT, "Ikon","Session",499);
+
+INSERT INTO passes
+VALUES (DEFAULT,"Epic","Full",1024), (DEFAULT,"Epic","Local",774), (DEFAULT,"Epic","4-Day",422);
+
+INSERT INTO passes
+VALUES (DEFAULT,"Mountain Collective","Full",589);
+
+INSERT INTO passes
+VALUES (DEFAULT,"Indy Pass","Adult",299), (DEFAULT,"Indy Pass","No Blackout",399);
